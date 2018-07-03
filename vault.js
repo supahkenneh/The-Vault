96b2396a2152cdc67f0d4a34cdaa7156f22fa0c8
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-07-02 15:32:51 -1000

    null if no key is used

--- vault.js
+++ vault.js
@@ -1,4 +1,19 @@
 'use strict';
 module.exports = function() {
-  
+  return {
+      setValue: setValue,
+      getValue: getValue,
+  }
+
+  function setValue(key, value){
+    var value = key;
+  }
+
+  function getValue(key){
+    if(!!key){
+        return value;
+    }else{
+        return null;
+    }
+  }
 };
